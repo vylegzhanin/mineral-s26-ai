e85bdd91e49ff7c4196f8fa3183cb1070dccfd05
Alexei Vylegzhanin <vylegzhanin@users.noreply.github.com>
2026-04-13 17:53:55 +0500

    Перенести @Push в AppShellConfigurator

---
+++ app-ui-vaadin/src/main/kotlin/com/example/ui/AppShell.kt
@@ -0,0 +1,7 @@
+package com.example.ui
+
+import com.vaadin.flow.component.page.AppShellConfigurator
+import com.vaadin.flow.component.page.Push
+
+@Push
+class AppShell : AppShellConfigurator
--- app-ui-vaadin/src/main/kotlin/com/example/ui/MainView.kt
+++ app-ui-vaadin/src/main/kotlin/com/example/ui/MainView.kt
@@ -14,7 +14,6 @@ import com.vaadin.flow.component.notification.NotificationVariant
 import com.vaadin.flow.component.orderedlayout.FlexComponent
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout
 import com.vaadin.flow.component.orderedlayout.VerticalLayout
-import com.vaadin.flow.component.page.Push
 import com.vaadin.flow.component.splitlayout.SplitLayout
 import com.vaadin.flow.component.textfield.NumberField
 import com.vaadin.flow.component.textfield.TextArea
@@ -37,7 +36,6 @@ import kotlin.concurrent.thread
 import javax.imageio.ImageIO
 
 @Route("")
-@Push
 class MainView : VerticalLayout() {
 
     private val datasetsRoot: Path = Path.of("/siams/images")
